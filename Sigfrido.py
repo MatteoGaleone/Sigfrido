00d54f1f4bb3ed3bdd61a8012e4c87d833b98152
MatteoGaleone <69458316+MatteoGaleone@users.noreply.github.com>
2022-02-07 13:34:18 +0100

    Add files via upload

--- Sigfrido.py
+++ Sigfrido.py
@@ -1,83 +1,114 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import yahoo_fin.stock_info as yf
 import pandas as pd
-from pandas_datareader import data
 import plotly.express as px
 
+
 def tickers():
-    t=[]
-    nt=int(input('Inserire numero di aziende da confrontare:'))
+    t = []
+    #t =yf.tickers_ftse100()
+    nt = int(input('Inserire numero di aziende da confrontare:'))
     for i in range(nt):
-        ticker=str(input('''Inserisci ticker dell'impresa numero ''' + str(i) +':'))
+        ticker = str(input('''Inserisci ticker dell'impresa numero ''' + str(i) + ':'))
         t.append(ticker)
     return t
 
+
 def correggi(stato_patrimoniale):
-    stato_patrimoniale=stato_patrimoniale.fillna(0)
-    try:
-        stato_patrimoniale['totalAssets']
-    except:
-        stato_patrimoniale['totalAssets']=[0,0,0,0]
-    try:
-        stato_patrimoniale['totalLiab']
-    except:
-        stato_patrimoniale['totalLiab']=[0,0,0,0]
+    stato_patrimoniale = stato_patrimoniale.fillna(0)
+
     try:
         stato_patrimoniale['longTermDebt']
     except:
-        stato_patrimoniale['longTermDebt']=[0,0,0,0]
+        stato_patrimoniale['longTermDebt'] = [0, 0, 0, 0]
     try:
         stato_patrimoniale['shortLongTermDebt']
     except:
-        stato_patrimoniale['shortLongTermDebt']=[0,0,0,0]
+        stato_patrimoniale['shortLongTermDebt'] = [0, 0, 0, 0]
+    try:
+        stato_patrimoniale['cash']
+    except:
+        stato_patrimoniale['cash'] = [0, 0, 0, 0]
+    try:
+        stato_patrimoniale['shortTermInvestments']
+    except:
+        stato_patrimoniale['shortTermInvestments'] = [0, 0, 0, 0]
+    try:
+        stato_patrimoniale['preferredStock']
+    except:
+        stato_patrimoniale['preferredStock'] = [0, 0, 0, 0]
 
     return stato_patrimoniale
-    
-    
-def Calcolo_ROIC_FAUSTMANN (t):
-    ROIC=[]
-    FAUSTMANN=[]
-    s=[0,0,0,0]
-    sp1=pd.DataFrame()
-    i=0
-    
+
+
+def Calcolo_ROIC_FAUSTMANN(t):
+    ROIC = []
+    FAUSTMANN = []
+    lista = []
+
     for x in t:
-        sp1=pd.DataFrame()
-        stato_patrimoniale=yf.get_balance_sheet(x)
-        stato_patrimoniale=stato_patrimoniale.T
-        stato_patrimoniale=correggi(stato_patrimoniale)
-        sp1.insert(0, 'Book Value Equity', stato_patrimoniale['totalAssets']-stato_patrimoniale['totalLiab'])
-        sp1.insert(0, 'Debt', stato_patrimoniale['longTermDebt']+stato_patrimoniale['shortLongTermDebt'])
-        sp1.insert(0, 'Invested Capital', sp1['Book Value Equity']+sp1['Debt'])
-    
-        conto_economico=yf.get_income_statement(x)
-        sp1.insert(0, 'ebit', conto_economico.T['ebit'])
-        
-        mc=data.get_quote_yahoo(x)['marketCap']
-        s[0]=mc[0]
-        sp1.insert(0, 'Market Cap', s)
-    
-        ROIC.append(sp1.iloc[0,1]/sp1.iloc[1,2])
-        FAUSTMANN.append(sp1.iloc[0,0]/sp1.iloc[0,4])
-    
-    
-    Data={'Ticker':t, 'ROIC':ROIC, 'FAUSTMANN':FAUSTMANN}
-    SIGFRIDI=pd.DataFrame(Data)
+
+        try:
+            stato_patrimoniale = yf.get_balance_sheet(x)
+            stato_patrimoniale = stato_patrimoniale.T
+            stato_patrimoniale = correggi(stato_patrimoniale)
+
+            totalAssets = stato_patrimoniale['totalAssets']
+            totalLiab = stato_patrimoniale['totalLiab']
+            longTermDebt = stato_patrimoniale['longTermDebt']
+            shortLongTermDebt = stato_patrimoniale['shortLongTermDebt']
+            totalStockholderEquity = stato_patrimoniale['totalStockholderEquity']
+            cash = stato_patrimoniale['cash'] + stato_patrimoniale['shortTermInvestments']
+            preferredStock = stato_patrimoniale['preferredStock']
+
+            BookValueEquity = totalAssets.iloc[0] - totalLiab.iloc[0]
+            Debt = longTermDebt.iloc[0] + shortLongTermDebt.iloc[0]
+            InvestedCapital = totalStockholderEquity[0] + Debt - cash[0]
+
+            conto_economico = yf.get_income_statement(x)
+            ebit = conto_economico.T['ebit']
+            ROIC.append(ebit[0] / InvestedCapital)
+
+            MC = yf.get_quote_table(x, dict_result=False).iloc[11, 1]
+            if MC[-1] == "B":
+                MC = MC.replace("B", "")
+                MC = float(MC)
+                MC = MC * 1000000000
+            elif MC[-1] == "M":
+                MC = MC.replace("M", "")
+                MC = float(MC)
+                MC = MC * 1000000
+            else:
+                MC = MC.replace("T", "")
+                MC = float(MC)
+                MC = MC * 1000000000000
+
+            FAUSTMANN.append(MC / totalStockholderEquity[0])
+        except:
+            lista.append(x)
+            continue
+
+    for x in lista:
+        t.remove(x)
+
+    Data = {'Ticker': t, 'ROIC': ROIC, 'FAUSTMANN': FAUSTMANN}
+    SIGFRIDI = pd.DataFrame(Data)
+
     return SIGFRIDI
-        
+
 
 def Grafico(SIGFRIDI):
-    fig = px.scatter(SIGFRIDI, x="ROIC", y="FAUSTMANN", 
-                 color='Ticker')
+    fig = px.scatter(SIGFRIDI, x="ROIC", y="FAUSTMANN",
+                     color='Ticker')
     return fig.show()
 
 
 def Programma():
     while True:
-        t=tickers()
-        SIGFRIDI=Calcolo_ROIC_FAUSTMANN(t)
+        t = tickers()
+        SIGFRIDI = Calcolo_ROIC_FAUSTMANN(t)
+        excel = pd.ExcelWriter('AZIONI.xlsx', engine='xlsxwriter')
+        SIGFRIDI.to_excel(excel, sheet_name='Azione')
+        excel.save()
         Grafico(SIGFRIDI)
         while True:
             risposta = str(input('Vuoi visualizzare un nuovo grafico? (si/no): '))
@@ -92,4 +123,3 @@ def Programma():
 
 
 Programma()
-
