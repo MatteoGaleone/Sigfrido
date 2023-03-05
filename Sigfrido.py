852d83d08145201f42e7d3a3eaf65cf5a5433f5f
MatteoGaleone <69458316+MatteoGaleone@users.noreply.github.com>
2023-03-05 16:05:36 +0100

    Update Sigfrido.py
    FAUSTMANN ratio is pretty difficult to calculate with the data avaible. Since comapring ROIC with PB is wrog i prefered to compare PB and ROE since both are measuring the equity.

--- Sigfrido.py
+++ Sigfrido.py
@@ -12,76 +12,22 @@ def tickers():
         t.append(ticker)
     return t
 
-
-def correct(balance_sheet):
-    balance_sheet = balance_sheet.fillna(0)
-
-    try:
-        balance_sheet['longTermDebt']
-    except:
-        balance_sheet['longTermDebt'] = [0, 0, 0, 0]
-    try:
-        balance_sheet['shortLongTermDebt']
-    except:
-        balance_sheet['shortLongTermDebt'] = [0, 0, 0, 0]
-    try:
-        balance_sheet['cash']
-    except:
-        balance_sheet['cash'] = [0, 0, 0, 0]
-    try:
-        balance_sheet['shortTermInvestments']
-    except:
-        balance_sheet['shortTermInvestments'] = [0, 0, 0, 0]
-    try:
-        balance_sheet['preferredStock']
-    except:
-        balance_sheet['preferredStock'] = [0, 0, 0, 0]
-
-    return balance_sheet
-
-
-def Calculation_ROIC_FAUSTMANN(t):
-    ROIC = []
-    FAUSTMANN = []
+def Calculation_ROE_PB(t):
+    ROE = []
+    PB = []
     lista = []
 
     for x in t:
-
         try:
-            balance_sheet = yf.get_balance_sheet(x)
-            balance_sheet = balance_sheet.T
-            balance_sheet = correct(balance_sheet)
-            totalAssets = balance_sheet['totalAssets']
-            totalLiab = balance_sheet['totalLiab']
-            longTermDebt = balance_sheet['longTermDebt']
-            shortLongTermDebt = balance_sheet['shortLongTermDebt']
-            totalStockholderEquity = balance_sheet['totalStockholderEquity']
-            cash = balance_sheet['cash'] + balance_sheet['shortTermInvestments']
-            preferredStock = balance_sheet['preferredStock']
-
-            BookValueEquity = totalAssets.iloc[0] - totalLiab.iloc[0]
-            Debt = longTermDebt.iloc[0] + shortLongTermDebt.iloc[0]
-            InvestedCapital = totalStockholderEquity[0] + Debt - cash[0]
-
-            income_statement = yf.get_income_statement(x)
-            ebit = income_statement.T['ebit']
-            ROIC.append(ebit[0] / InvestedCapital)
-
-            MarketCap = yf.get_quote_table(x, dict_result=False).iloc[11, 1]
-            if MarketCap[-1] == "B":
-                MarketCap = MarketCap.replace("B", "")
-                MarketCap = float(MarketCap)
-                MarketCap = MarketCap * 1000000000
-            elif MarketCap[-1] == "M":
-                MarketCap = MarketCap.replace("M", "")
-                MarketCap = float(MarketCap)
-                MarketCap = MarketCap * 1000000
-            else:
-                MarketCap = MarketCap.replace("T", "")
-                MarketCap = float(MarketCap)
-                MarketCap = MarketCap * 1000000000000
-
-            FAUSTMANN.append(MarketCap / totalStockholderEquity[0])
+            Return_on_equity=yf.get_stats(x).iloc[34,1]
+            if Return_on_equity[-1] == "%":
+                Return_on_equity = Return_on_equity.replace("%", "")
+                Return_on_equity = float(Return_on_equity)
+            ROE.append(Return_on_equity)
+            
+            Price_to_book_ratio=float(yf.get_stats_valuation(x).iloc[6,1])
+            PB.append(Price_to_book_ratio)
+            
         except:
             lista.append(x)
             continue
@@ -89,14 +35,13 @@ def Calculation_ROIC_FAUSTMANN(t):
     for x in lista:
         t.remove(x)
 
-    Data = {'Ticker': t, 'ROIC': ROIC, 'FAUSTMANN': FAUSTMANN}
+    Data = {'Ticker': t, 'Return_on_equity': ROE, 'Price_to_book_ratio': PB}
     SIGFRIDS = pd.DataFrame(Data)
-
     return SIGFRIDS
 
 
 def Graphic(SIGFRIDS):
-    fig = px.scatter(SIGFRIDS, x="ROIC", y="FAUSTMANN",
+    fig = px.scatter(SIGFRIDS, x="Return_on_equity", y="Price_to_book_ratio",
                      color='Ticker')
     return fig.show()
 
@@ -104,11 +49,12 @@ def Graphic(SIGFRIDS):
 def Program():
     while True:
         t = tickers()
-        SIGFRIDS = Calculation_ROIC_FAUSTMANN(t)
+        SIGFRIDS = Calculation_ROE_PB(t)
         excel = pd.ExcelWriter('Sigfrids.xlsx', engine='xlsxwriter')
         SIGFRIDS.to_excel(excel, sheet_name='Stocks')
         excel.save()
         Graphic(SIGFRIDS)
+        print(SIGFRIDS)
         while True:
             response = str(input('Do you want to visualize a new Graphic? (yes/no): '))
             if response in ('yes', 'no'):
@@ -119,6 +65,5 @@ def Program():
         else:
             print('Bye')
             break
-
 
 Program()
